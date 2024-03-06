cd6c719d56cebd7056759acd9a356dd60903a9e7
Vitaly <vitaliy.zayakin@gmail.com>
2024-03-06 13:31:02 +0500

    feat: add dotenv to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
+  const PORT = Number(process.env.PORT) || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(PORT, () => console.log('Server started on port: ' + PORT));
 }
 bootstrap();
